df95e1d0e2c4dea92e1d410d72966eae4236b3f7
FavourMaca <favouronyekwere830@gmail.com>
2022-09-08 20:05:03 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,8 @@
 
 /**
  * main - Entry point
- * Return: 0 if successful
- *
- * Description: This prints with proper grammar, but the outcome is a piece of 
- *art,
+ * Return: Always 0 (Success)
  */
-
 
 int main (void)
 {
