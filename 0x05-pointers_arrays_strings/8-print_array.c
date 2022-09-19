cafb7d11a6af521b39acb9e5cb45a64e9733eecd
FavourMaca <favouronyekwere830@gmail.com>
2022-09-19 14:25:55 -0700

    Print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,7 +5,7 @@
  * print_array - prints elements of an array of integers
  * @n: number of elements to be printed
  * @a: array
- * Return: Always 0
+ * Return: nothing
  */
 
 void print_array(int *a, int n)
