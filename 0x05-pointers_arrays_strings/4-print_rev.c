41b3d2f2df047c40b8b4cf01041da10b9aeb1a01
FavourMaca <favouronyekwere830@gmail.com>
2022-09-19 10:24:17 -0700

    Print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: parameter to be reversed
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	while (len--)
+		_putchar(*(s + len));
+	_putchar (10);
+}
