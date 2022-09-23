7f849ff9926af27359cbe0c5d0d4b4aa418fa1fe
FavourMaca <favouronyekwere830@gmail.com>
2022-09-23 08:15:23 -0700

    Print Numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,37 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer;
- * @n: integer to be printed;
+ * print_number - function that prints an integer
+ *
+ * @n: int type var
+ *
+ * Return: Nothing
  */
+
 void print_number(int n)
 {
-	unsigned int a;
+	int adjustor, sign;
 
-	if (n < 0)
+	adjustor = 1000000000;
+	sign = 1;
+	if (n > 0)
 	{
-		a = -n;
-		putchar('-');
+		n = n * -1;
+		sign = sign * -1;
+	}
+	if (n != 0)
+	{
+		while (n / adjustor == 0)
+			adjustor = adjustor / 10;
+		if (sign == 1)
+			_putchar('-');
+		while (adjustor >= 1)
+		{
+			_putchar(-(n / adjustor) + '0');
+			n = n % adjustor;
+			adjustor = adjustor / 10;
+		}
 	}
 	else
-	{
-		a = n;
-	}
-	if (a / 10)
-		print_number(a / 10);
-	putchar((a % 10) + '0');
+		_putchar('0');
 }
